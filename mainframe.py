c3e6c8e2f31aca544fe0f871219c118368e98f4b
Allan Sung (RD-TW) <allan_sung@tw.trendnet.org>
2015-12-30 11:27:37 +0800

    Python with ListView Data and click event

--- mainframe.py
+++ mainframe.py
@@ -24,6 +24,17 @@ class Ui_MainWindow(object):
         self.listView.setGeometry(QtCore.QRect(20, 10, 421, 321))
         self.listView.setStyleSheet("font: 75 italic 9pt \"Agency FB\";")
         self.listView.setObjectName("listView")
+        # Create an empty model for the list's data
+        self.model = QtGui.QStandardItemModel(self.listView)
+        self.item = QtGui.QStandardItem("Item 1")
+        # Add a checkbox to it
+        self.item.setCheckable(True)
+        # Add the item to the model
+        self.model.appendRow(self.item)
+        self.model.itemChanged.connect(self.on_item_changed)
+        # Apply the model to the list view
+        self.listView.setModel(self.model)
+
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
         self.tableWidget.setGeometry(QtCore.QRect(510, 10, 256, 321))
         self.tableWidget.setObjectName("tableWidget")
@@ -67,3 +78,17 @@ class Ui_MainWindow(object):
         dialog.ui.setupUi(dialog)
         dialog.exec_()
 
+    def on_item_changed(self, item):
+        # If the changed item is not checked, don't bother checking others
+        if not item.checkState():
+            return
+     
+        # Loop through the items until you get None, which
+        # means you've passed the end of the list
+        i = 0
+        while self.model.item(i):
+            if not self.model.item(i).checkState():
+                return
+            print(self.model.item(i).text())
+            i += 1
+        print(i)
